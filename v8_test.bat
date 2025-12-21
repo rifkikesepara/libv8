04155878a5d06d371629e23e357031f059ed6058
Rıfkı Kesepara <rifkikesepara7@gmail.com>
2025-12-22 00:05:23 +0300

    Update v8_test.bat

--- v8_test.bat
+++ v8_test.bat
@@ -9,11 +9,11 @@ if not exist "%dir%\v8" (
   exit /b 1
 )
 
-call cl.exe /clr /std:c++20 /Zc:__cplusplus ^
+call cl.exe /std:c++20 /Zc:__cplusplus ^
   /I"%dir%\v8" /I"%dir%\v8\include" ^
   /Fe".\hello-world" "%dir%\v8\samples\hello-world.cc" ^
   /link "%dir%\v8\out\release\obj\v8_monolith.lib" ^
-  /DEFAULTLIB:advapi32.lib /DEFAULTLIB:dbghelp.lib /DEFAULTLIB:winmm.lib
+  advapi32.lib dbghelp.lib winmm.lib ws2_32.lib user32.lib ole32.lib shell32.lib version.lib
 
 if errorlevel 1 (
   echo Compilation failed
